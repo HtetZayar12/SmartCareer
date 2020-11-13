201711b23d518e69144ebd176f22f2c19aecce70
aunghtunwin1 <aungtunwin538@gmail.com>
2020-11-13 19:00:08 +0630

    fix error

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('frontend/signin');
-})->name;
+});
 Route::get('/signup', function () {
     return view('frontend/signup');
 });
