3ed7ba85ed676e21e3ecfc6fe222939ecf7a8b36
aunghtunwin1 <aungtunwin538@gmail.com>
2020-11-13 17:36:34 +0630

    added signin and sigup

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('frontend/signin');
+})->name;
+Route::get('/signup', function () {
+    return view('frontend/signup');
 });
