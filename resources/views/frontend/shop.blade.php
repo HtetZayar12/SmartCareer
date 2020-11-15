9d60d5f09722263e0174a86b205c81866039bd06
aunghtunwin1 <aungtunwin538@gmail.com>
2020-11-15 11:29:47 +0630

    added howitwork and shop blade

---
+++ resources/views/frontend/shop.blade.php
@@ -0,0 +1,52 @@
+@extends('frontendtemplate')
+@section('content')
+ <div class="banner">
+     <img src="assets/img/E-Books Etsy Banner (1).png" alt="" class="img-fluid w-100 mb-5">
+ </div>
+ <div class="container" style="font-family: Montserrat, sans-serif;">
+     <div class="row">
+         <div class="col-lg-4 col-sm-12">
+             <div class="card text-center">
+                 <div class="card-header">
+                   <div class="font-weight-bold">100 Coins</div>
+                 </div>
+                 <div class="card-body">
+                   <h5 class="card-title">10000MMK</h5>
+                   <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                 </div>
+                 <div class="card-footer text-muted">
+                     <a href="#" class="btn">Buy</a>
+                 </div>
+               </div>
+         </div>
+         <div class="col-lg-4 col-sm-12">
+             <div class="card text-center">
+                 <div class="card-header">
+                   <div class="font-weight-bold">250 Coins</div>
+                 </div>
+                 <div class="card-body">
+                   <h5 class="card-title">25000MMK</h5>
+                   <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                 </div>
+                 <div class="card-footer text-muted">
+                     <a href="#" class="btn">Buy</a>
+                 </div>
+               </div>
+         </div>
+         <div class="col-lg-4 col-sm-12">
+             <div class="card text-center">
+                 <div class="card-header">
+                   <div class="font-weight-bold">500 Coins</div>
+                 </div>
+                 <div class="card-body">
+                   <h5 class="card-title">50000MMK</h5>
+                   <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                 </div>
+                 <div class="card-footer text-muted">
+                     <a href="#" class="btn">Buy</a>
+                 </div>
+               </div>
+         </div>
+     </div>
+ </div>
+@endsection
